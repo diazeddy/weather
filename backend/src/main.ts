99c0845aa881d043d7e253e00aa5761045ce272c
Eddy Diaz <eddydiaz0219@gmail.com>
2025-03-30 14:36:11 -0500

    make the backend accessible from host macine via docker

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,8 @@
 import app from "./app";
 
 const startServer = async () => {
-  app.listen(5000, "127.0.0.1", () => {
-    console.log("Server is running at http://127.0.0.1:5000.");
+  app.listen(5000, '0.0.0.0', () => {
+    console.log(`Server is running at http://0.0.0.0:5000`);
   });
 };
 
